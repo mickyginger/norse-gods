c567e9ead6e07307fa62e2417c57840e5b5b03a2
Gabe Naughton <gabriel.naughton135@gmail.com>
2019-08-16 10:50:37 +0100

    all routes working, 401 tests failing for update and delete

--- config/routes.js
+++ config/routes.js
@@ -4,10 +4,10 @@ const figuresController = require('../controllers/figures')
 const secureRoute = require('../lib/secureRoute')
 
 router.get('/figures', figuresController.index)
-router.post('/figures', figuresController.create)
+router.post('/figures', secureRoute, figuresController.create)
 router.get('/figures/:id', figuresController.show)
-router.put('/figures/:id', figuresController.update)
-router.delete('/figures/:id', figuresController.delete)
+router.put('/figures/:id', secureRoute, figuresController.update)
+router.delete('/figures/:id', secureRoute, figuresController.delete)
 
 router.post('/register', auth.register)
 router.post('/login', auth.login)
